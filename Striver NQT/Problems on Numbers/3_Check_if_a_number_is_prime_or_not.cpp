609f03d14785bab59e0001ae821d9fa605c77a07
karakRohan <karakrohan2005@gmail.com>
2026-05-21 23:54:57 +0530

    add

---
+++ Striver NQT/Problems on Numbers/3_Check_if_a_number_is_prime_or_not.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void primeNumber(int n)
+{
+    int c = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        c += (n % i == 0);
+    }
+
+    cout << (c == 2 ? "True" : "False");
+}
+
+int main(){
+
+    string s;
+    getline(cin,s);
+
+    vector<int> arr;
+
+    stringstream ss(s);
+
+    string temp;
+
+    while(getline(ss,temp,' ')){
+        arr.push_back(stoi(temp));
+    }
+
+    primeNumber(arr[0]);
+
+    return 0;
+}
