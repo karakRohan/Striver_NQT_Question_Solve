603fd6b9bf17f32f2e8310eb430f235dc112258b
karakRohan <karakrohan2005@gmail.com>
2026-05-29 01:20:14 +0530

    add

---
+++ Striver NQT/Problems on Numbers/16_Maximum_and_Minimum_Digit_in_a_Number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void maxMinDigit(int n)
+{
+    string s = to_string(n);
+
+    cout << *max_element(s.begin(),s.end())-'0'
+         << " "
+        << *min_element(s.begin(),s.end())-'0';
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    vector<int> arr;
+    stringstream ss(s);
+    string temp;
+    while(getline(ss,temp,' ')){
+        arr.push_back(stoi(temp));
+    }
+    maxMinDigit(arr[0]);
+    return 0;
+}
