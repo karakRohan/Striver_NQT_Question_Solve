bd64e2b365e080a4b228c80d1c9832c97939823f
karakRohan <karakrohan2005@gmail.com>
2026-06-01 02:05:13 +0530

    LCM add

---
+++ Striver NQT/Problems on Numbers/25_Find_LCM_of_two_numbers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void lcmNumber(int a, int b)
+{
+    cout << (a*b)/__gcd(a,b);
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    vector<int> arr;
+    stringstream ss(s);
+    string temp;
+    while(getline(ss,temp,' ')){
+        arr.push_back(stoi(temp));
+    }
+    lcmNumber(arr[0], arr[1]);
+    return 0;
+}
