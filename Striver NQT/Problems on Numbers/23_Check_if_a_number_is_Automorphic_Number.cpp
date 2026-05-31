c573c7d77867f2bc387f618a9781ca7cc92585e2
karakRohan <karakrohan2005@gmail.com>
2026-06-01 01:58:00 +0530

    Check if a number is Automorphic Number

---
+++ Striver NQT/Problems on Numbers/23_Check_if_a_number_is_Automorphic_Number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void automorphic(int n)
+{
+    string a = to_string(n);
+    string b = to_string(n*n);
+    cout << (b.substr(b.size()-a.size()) == a ?
+            "Automorphic Number" :
+            "Not Automorphic Number");
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    vector<int> arr;
+    stringstream ss(s);
+    string temp;
+    while(getline(ss,temp,' ')){
+        arr.push_back(stoi(temp));
+    }
+    automorphic(arr[0]);
+    return 0;
+}
