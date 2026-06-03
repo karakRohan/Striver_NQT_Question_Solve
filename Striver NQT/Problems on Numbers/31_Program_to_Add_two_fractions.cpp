e2d77f014237c2076de7c6d87e9567d96f667736
karakRohan <karakrohan2005@gmail.com>
2026-06-04 01:21:12 +0530

    Program to Add two fractions

---
+++ Striver NQT/Problems on Numbers/31_Program_to_Add_two_fractions.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void addFraction(int n1,int d1,int n2,int d2)
+{
+    int n=n1*d2+n2*d1,
+        d=d1*d2,
+        g=__gcd(n,d);
+    cout<<n/g<<" "<<d/g;
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    vector<int> arr;
+    stringstream ss(s);
+    string temp;
+    while(getline(ss,temp,' ')){
+        arr.push_back(stoi(temp));
+    }
+    addFraction(arr[0],arr[1],arr[2],arr[3]);
+    return 0;
+}
