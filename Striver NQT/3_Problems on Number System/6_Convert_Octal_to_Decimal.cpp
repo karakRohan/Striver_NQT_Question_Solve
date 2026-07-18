130aea07438e774f95340a87af0ae703aa23602c
karakRohan <karakrohan2005@gmail.com>
2026-07-18 20:49:59 +0530

    Convert Octal to Decimal

---
+++ Striver NQT/3_Problems on Number System/6_Convert_Octal_to_Decimal.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void octalToDecimal(string s)
+{
+    cout << stoi(s,0,8);
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    vector<string> arr;
+    stringstream ss(s);
+    string temp;
+    while(getline(ss,temp,' ')){
+        arr.push_back(temp);
+    }
+    octalToDecimal(arr[0]);
+    return 0;
+}
